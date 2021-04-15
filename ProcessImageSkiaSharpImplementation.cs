c6288a8781d413e0743ab77883cb61b16fd13bc4
Matheus Dadalto <mth_dadalto@hotmail.com>
2021-04-15 18:41:55 +0100

    Add project files.

---
+++ ProcessImageSkiaSharpImplementation.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+using SkiaSharp;
+
+namespace TreadReader
+{
+    /// <summary>
+    /// ProcessImage SkiaSharp 2.80.x Implementation
+    /// </summary>
+    public class ProcessImageSkiaSharpImplementation : IProcessImage
+    {
+        public ProcessImage Convert(ProcessImage source, ProcessImageType target, bool flipHorizontal = false, bool flipVertical = false)
+        {
+            if (source.Type == target) { return source; }
+
+            if (source.Type == ProcessImageType.PNG) { return Encode(SKImage.FromBitmap(SKBitmap.Decode(source.Load)), target, flipHorizontal, flipVertical); }
+            if (source.Type == ProcessImageType.Grayscale8) { return Encode(SKImage.FromPixelCopy(new SKImageInfo(source.Width, source.Height, SKColorType.Gray8), source.Load), target, flipHorizontal, flipVertical); }
+            if (source.Type == ProcessImageType.RGBA8) { return Encode(SKImage.FromPixelCopy(new SKImageInfo(source.Width, source.Height, SKColorType.Rgba8888), source.Load), target, flipHorizontal, flipVertical); }
+            if (source.Type == ProcessImageType.RGBA16) { return Encode(SKImage.FromPixelCopy(new SKImageInfo(source.Width, source.Height, SKColorType.RgbaF16), source.Load), target, flipHorizontal, flipVertical); }
+
+            /*if (source.Type == ProcessImageType.RGBA32)
+            {
+                Image img = Image.LoadPixelData<RgbaVector>(source.Load, source.Width, source.Height);
+                if (flipHorizontal) { img.Mutate(x => x.Flip(FlipMode.Horizontal)); }
+                if (flipVertical) { img.Mutate(x => x.Flip(FlipMode.Vertical)); }
+                return Encode(img, target);
+            }*/
+
+            throw new FormatException("Format conversion not implemented");
+        }
+
+
+        public ProcessImage Encode(SKImage img, ProcessImageType toType, bool flipHorizontal, bool flipVertical)
+        {
+            if (flipHorizontal || flipVertical)
+            {
+                using SKBitmap bmp = new SKBitmap(img.Width, img.Height);
+                using SKCanvas surface = new SKCanvas(bmp);
+                surface.Scale(flipHorizontal ? -1 : 1, flipVertical ? -1 : 1, flipHorizontal ? img.Width / 2f : 0, flipVertical ? img.Height / 2f : 0);
+                surface.DrawImage(img, 0, 0);
+                img = SKImage.FromBitmap(bmp);
+            }
+
+            if (toType == ProcessImageType.JPEG)
+            {
+                return new ProcessImage { Load = img.Encode(SKEncodedImageFormat.Jpeg, 95).ToArray(), Type = toType, Width = img.Width, Height = img.Height };
+            }
+            if (toType == ProcessImageType.PNG)
+            {
+                return new ProcessImage { Load = img.Encode(SKEncodedImageFormat.Png, 95).ToArray(), Type = toType, Width = img.Width, Height = img.Height };
+            }
+            if (toType == ProcessImageType.RGBA8)
+            {
+                SKBitmap bmp = SKBitmap.FromImage(img);
+                return new ProcessImage { Load = bmp.Bytes, Height = bmp.Height, Type = toType, Width = bmp.Width };
+            }
+            throw new FormatException("Format conversion not implemented");
+        }
+/*
+        public ProcessImage Encode(Image img, ProcessImageType toType)
+        {
+            if (toType == ProcessImageType.JPEG)
+            {
+                using MemoryStream ms = new MemoryStream();
+                img.SaveAsJpeg(ms);
+                return new ProcessImage { Load = ms.ToArray(), Height = img.Height, Type = toType, Width = img.Width };
+            }
+            if (toType == ProcessImageType.PNG)
+            {
+                using MemoryStream ms = new MemoryStream();
+                img.SaveAsPng(ms);
+                return new ProcessImage { Load = ms.ToArray(), Height = img.Height, Type = toType, Width = img.Width };
+            }
+
+            if (toType == ProcessImageType.RGBA8)
+            {
+            }
+            if (toType == ProcessImageType.RGBA32)
+            {
+            }
+            throw new FormatException("Format conversion not implemented");
+        }*/
+
+        public ProcessImage PngToJpeg(ProcessImage source)
+        {
+            if(source.Type != ProcessImageType.PNG) { throw new FormatException("Process image should be Png"); }
+            SKImage tmp = SKImage.FromBitmap(SKBitmap.Decode(source.Load));
+            return new ProcessImage { Load = tmp.Encode(SKEncodedImageFormat.Jpeg, 95).ToArray(), Type = ProcessImageType.JPEG, Width = tmp.Width, Height = tmp.Height };
+        }
+
+        public ProcessImage PngToRgba8(ProcessImage source)
+        {
+            if (source.Type != ProcessImageType.PNG) { throw new FormatException("Process image should be Png"); }
+            SKBitmap bmp = SKBitmap.Decode(source.Load);
+            return new ProcessImage { Load = bmp.Bytes, Height = bmp.Height, Type = ProcessImageType.RGBA8, Width = bmp.Width };
+        }
+
+        public ProcessImage Rgba16ToPng(ProcessImage source, bool flipHorizontal, bool flipVertical)
+        {
+            if (source.Type != ProcessImageType.RGBA16) { throw new FormatException("Process image should be RGBA16"); }
+            using SKImage img = SKImage.FromPixelCopy(new SKImageInfo(source.Width, source.Height, SKColorType.Rgba16161616), source.Load);
+            using SKBitmap bmp = new SKBitmap(img.Width, img.Height);
+            using SKCanvas surface = new SKCanvas(bmp);
+            surface.Scale(flipHorizontal ? -1 : 1, flipVertical ? -1 : 1, flipHorizontal ? source.Width / 2f : 0, flipVertical ? source.Height / 2f : 0);
+            surface.DrawImage(img, 0, 0);
+
+            return new ProcessImage { Load = SKImage.FromBitmap(bmp).Encode(SKEncodedImageFormat.Png, 99).ToArray(), Height = source.Height, Type = ProcessImageType.PNG, Width = source.Width };
+        }
+
+        public ProcessImage Rgba32ToPng(ProcessImage source, bool flipHorizontal, bool flipVertical)
+        {
+            if (source.Type != ProcessImageType.RGBA32) { throw new FormatException("Process image should be RGBA32"); }
+            using SKImage img = SKImage.FromPixelCopy(new SKImageInfo(source.Width, source.Height, SKColorType.RgbaF32), source.Load);
+            using SKBitmap bmp = new SKBitmap(img.Width, img.Height);
+            using SKCanvas surface = new SKCanvas(bmp);
+            surface.Scale(flipHorizontal ? -1 : 1, flipVertical ? -1 : 1, flipHorizontal ? source.Width / 2f : 0, flipVertical ? source.Height / 2f : 0);
+            surface.DrawImage(img, 0, 0);
+
+            return new ProcessImage { Load = SKImage.FromBitmap(bmp).Encode(SKEncodedImageFormat.Png, 99).ToArray(), Height = source.Height, Type = ProcessImageType.PNG, Width = source.Width };
+        }
+
+        public ProcessImage Rgba8ToPng(ProcessImage source, bool flipHorizontal, bool flipVertical)
+        {
+            if (source.Type != ProcessImageType.RGBA8) { throw new FormatException("Process image should be RGBA8"); }
+            using SKImage img = SKImage.FromPixelCopy(new SKImageInfo(source.Width, source.Height, SKColorType.Rgba8888), source.Load);
+            using SKBitmap bmp = new SKBitmap(img.Width, img.Height);
+            using SKCanvas surface = new SKCanvas(bmp);
+            surface.Scale(flipHorizontal ? -1 : 1, flipVertical ? -1 : 1, flipHorizontal ? source.Width / 2f : 0, flipVertical ? source.Height / 2f : 0);
+            surface.DrawImage(img, 0, 0);
+            return new ProcessImage { Load = SKImage.FromBitmap(bmp).Encode(SKEncodedImageFormat.Png, 99).ToArray(), Height = source.Height, Type = ProcessImageType.PNG, Width = source.Width };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+using TreadManager;
+
+using TreadReader;
+using TreadReader.HandHeld;
+using TreadReader.Render;
+
+namespace HandheldSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connect to your Handheld Wifi and press any key to continue");
+            Console.ReadKey();
+
+            //required as platform image driver
+            Helpers.ImageProcessing = new ProcessImageSkiaSharpImplementation();
+
+            //Initialize the Hanhheld driver and pass a renderer to produce PNGs
+            DeviceDriver handheld = new(VeldridRender.InitFromVulkan());
+
+            //Try to link to device in direct mode (must bu connected to device WIFI)
+            handheld.Connect();
+
+            Console.WriteLine("Status = " + (handheld.Connected ? "Connected" : "Not Connected"));
+
+            //Keep rescanning until disconnected
+            while (handheld.Connected)
+            {
+                Console.WriteLine("Input the VRN:");
+                string vrn = Console.ReadLine();
+                //Making a Scan is Optional, you can use a syngle Tyre obj like handheld.StartScan(new Tyre()); should you want
+                Scan tmpScan = new(vrn);
+
+                tmpScan.AddTyre(0, 1, 0, "Front Left");
+                tmpScan.AddTyre(0, 2, 0, "Front Right");
+                //OR
+                tmpScan.Tyres.Add(new Tyre() { Axle = 1, Index = 0, Hub = 1, Placement = "Rear Left" });
+                tmpScan.Tyres.Add(new Tyre() { Axle = 1, Index = 0, Hub = 2, Placement = "Rear Right" });
+
+                Console.WriteLine("Scanning " + vrn);
+                //Scan through vehicle
+                foreach (Tyre tmpTyre in tmpScan.Tyres)
+                {
+                    if (tmpTyre.Axle == 1) { tmpTyre.Meta.Season = TyreSeason.winter; }//Set winter tyres for rear
+                    while (tmpTyre.ScanningIssue != ScanIssue.None)
+                    {
+                        Thread.Sleep(500);
+                        Console.WriteLine("Use the handheld to scan the " + tmpTyre.Placement);
+                        handheld.StartScan(tmpTyre);
+                        if (tmpTyre.ScanningIssue != ScanIssue.None)
+                        {
+                            Console.WriteLine(tmpTyre.ScanningIssue.ToString());
+                        }
+                        else
+                        {
+                            //GetRender process all necessary steps to get depths and corrections and return a png
+                            File.WriteAllBytes(Path.GetFullPath("./" + tmpScan.Vehicle.VRN + "_" + tmpTyre.Axle + "_" + tmpTyre.Hub + ".png"), tmpTyre.GetRender()); // Save the png to file
+
+                            //Some output
+                            Console.WriteLine("Result: " + Helpers.TreadDepthResult(tmpTyre.MinDepth, tmpTyre.Meta.Season).ToString());
+                            Console.WriteLine("Depths: " + string.Join(", ", tmpTyre.Depths));
+                            Console.WriteLine("Has defects: " + (tmpTyre.Inflation != 0 || tmpTyre.Misalignment != 0).ToString());
+
+                        }
+                    }
+                }
+
+            }
+            Console.WriteLine("Device Disconnected");
+        }
+    }
+}
